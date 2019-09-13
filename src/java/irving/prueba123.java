2c537299ad9f11f78ac0b44e659c8d1d18f72e73
LAB-USR-CAQP-C0203 <LAB-USR-CAQP-C0203@UTP215197.grupoutp.net>
2019-09-12 19:54:36 -0500

    preuba2

---
+++ src/java/irving/prueba123.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package irving;
+
+/**
+ *
+ * @author LAB-USR-CAQP-C0203
+ */
+public class prueba123 {
+    
+}
